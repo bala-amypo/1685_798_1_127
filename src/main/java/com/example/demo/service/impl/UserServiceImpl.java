66148a6096cb8b086c56f38ee6f97bd6b4674441
bala-amypo <bala@amypo.in>
2025-12-18 08:53:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.demo.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -14,18 +16,27 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder encoder;
     private final JwtUtil jwtUtil;
 
-    public UserServiceImpl(UserRepository repo, PasswordEncoder encoder, JwtUtil jwtUtil) {
+    public UserServiceImpl(UserRepository repo,
+                           PasswordEncoder encoder,
+                           JwtUtil jwtUtil) {
         this.repo = repo;
         this.encoder = encoder;
         this.jwtUtil = jwtUtil;
     }
 
+    @Override
     public User saveUser(User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 
+    @Override
     public User findByEmail(String email) {
         return repo.findByEmail(email);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return repo.findById(id);
     }
 }
