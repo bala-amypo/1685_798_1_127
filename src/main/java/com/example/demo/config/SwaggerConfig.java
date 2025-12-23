c9411df6c1cdb9ab68803a9aae52955a14324cdf
bala-amypo <bala@amypo.in>
2025-12-23 04:51:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,14 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -16,22 +11,10 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme jwtScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
         return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Complaint Prioritization Engine")
-                        .description("Spring Boot + JWT + Swagger API Documentation")
-                        .version("1.0"))
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9101.408procr.amypo.ai/")
-                ))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", jwtScheme));
-    }
+                        new Server().url("https://9232.pro604cr.amypo.ai/")
+                ));
+        }
 }
